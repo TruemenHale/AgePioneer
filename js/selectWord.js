64af41156404fe78a8e1e5b79ecf8785c767cd9f
TruemenHale <truemenhale@gmail.com>
2016-05-22 01:29:55 +0800

    little

--- js/selectWord.js
+++ js/selectWord.js
@@ -1,11 +1,39 @@
 /**
  * Created by truemenhale on 16/5/17.
  */
-var num = 0;
-var qType = 0;
 $(function(){
-    var selections = $('.selections');
-    selections.find('li').on('tap',function(){
-
+    var playGame = $('.beginBtn');
+    var selectors = $('.selections').find('li');
+    var answerTwo = $('.answer2');
+    var answerThree = $('.answer3');
+    var aLiTwo = answerTwo.find('li');
+    var aLiThree = answerThree.find('li');
+    var Qn = 0;
+    var r = "";
+    var FlagThree = 0;
+    var FlagTwo = 0;
+    selectors.on('tap',function(){
+        if(parseInt(_data[Qn].nameLength) == 3){
+            aLiThree.eq(FlagThree).html($(this).html());
+            r += $(this).html();
+            FlagThree++;
+            if(FlagThree == 3){
+                if(r == _data[Qn].answer){
+                    console.log("1");
+                }else {
+                    console.log(0);
+                }
+                r = "";
+                FlagThree = 0;
+            }
+        }else{
+            console.log(_data[Qn]);
+        }
+    });
+    playGame.on('tap',function(){
+        ChangeQuestion(Qn,selectors);
+        $.mobile.changePage('#GamePage',{
+            transition : 'slide'
+        })
     });
 });
